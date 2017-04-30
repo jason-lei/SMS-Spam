03457be24ccb2dd2e5208974a3cb29cd208b1079
Andres Soto <ads2206@columbia.edu>
2017-04-30 15:01:08 -0400

    deleted SMS/gmail-api folder. it is now in SMS/app/gmail-api

--- gmail-api/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-#app.config.from_object('config') #tell Flask to read configuration file
-
-#from app import views, models
--- gmail-api/run.py
+++
@@ -1,4 +0,0 @@
-from app import app
-import quickstart
-quickstart.main() 
-#app.run(debug=True)
