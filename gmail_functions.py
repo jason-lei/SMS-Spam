0347b4281fbfd938d73ca6eb12705687120d3541
jason-lei <jason.lei@columbia.edu>
2017-05-09 22:36:49 -0400

    added gmail front end

---
+++ gmail_functions.py
@@ -0,0 +1,160 @@
+from __future__ import print_function
+
+import httplib2
+import os
+
+from apiclient import discovery, errors
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+import base64
+import email
+
+def GetMessage(service, user_id, msg_id):
+  """Get a Message with given ID.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: The ID of the Message required.
+
+  Returns:
+    A Message.
+  """
+  try:
+    message = service.users().messages().get(userId=user_id, id=msg_id, format='metadata').execute()
+
+    # print('Message snippet: %s' % message['snippet'])
+    return message
+  except errors.HttpError:
+    print('An error occurred: %s' % error)
+
+def GetMimeMessage(service, user_id, msg_id):
+  """Get a Message and use it to create a MIME Message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: The ID of the Message required.
+
+  Returns:
+    A MIME Message, consisting of data from Message.
+  """
+  try:
+    message = service.users().messages().get(userId=user_id, id=msg_id,
+                                             format='raw').execute()
+
+    #print('Message snippet: %s' % message['snippet'])
+
+    msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+
+    mime_msg = email.message_from_string(msg_str)
+
+    return mime_msg
+  except errors.HttpError:
+    print('An error occurred: %s' % error)
+
+def CreateLabel(service, user_id, label_object):
+  """Creates a new label within user's mailbox, also prints Label ID.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    label_object: label to be added.
+
+  Returns:
+    Created Label.
+  """
+  try:
+    label = service.users().labels().create(userId=user_id,
+                                            body=label_object).execute()
+    print(label['id'])
+    return label
+  except errors.HttpError:
+    print('An error occurred: %s' % error)
+
+
+def MakeLabel(label_name, mlv='show', llv='labelShow'):
+  """Create Label object.
+
+  Args:
+    label_name: The name of the Label.
+    mlv: Message list visibility, show/hide.
+    llv: Label list visibility, labelShow/labelHide.
+
+  Returns:
+    Created Label.
+  """
+  label = {'messageListVisibility': mlv,
+           'name': label_name,
+           'labelListVisibility': llv}
+  return label
+
+
+def ListMessagesMatchingQuery(service, user_id, query=''):
+  """List all Messages of the user's mailbox matching the query.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    query: String used to filter messages returned.
+    Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
+
+  Returns:
+    List of Messages that match the criteria of the query. Note that the
+    returned list contains Message IDs, you must use get with the
+    appropriate ID to get the details of a Message.
+  """
+  try:
+    response = service.users().messages().list(userId=user_id,
+                                               q=query, maxResults=50).execute()
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+      page_token = response['nextPageToken']
+      response = service.users().messages().list(userId=user_id, q=query,
+                                         pageToken=page_token).execute()
+      messages.extend(response['messages'])
+
+    return messages
+  except errors.HttpError:
+    print('An error occurred: %s' % error)
+
+
+def ListMessagesWithLabels(service, user_id, label_ids=[]):
+  """List all Messages of the user's mailbox with label_ids applied.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    label_ids: Only return Messages with these labelIds applied.
+
+  Returns:
+    List of Messages that have all required Labels applied. Note that the
+    returned list contains Message IDs, you must use get with the
+    appropriate id to get the details of a Message.
+  """
+  try:
+    response = service.users().messages().list(userId=user_id,
+                                               labelIds=label_ids).execute()
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+      page_token = response['nextPageToken']
+      response = service.users().messages().list(userId=user_id,
+                                                 labelIds=label_ids,
+                                                 pageToken=page_token).execute()
+      messages.extend(response['messages'])
+
+    return messages
+  except errors.HttpError:
+    print('An error occurred: %s' % error)
