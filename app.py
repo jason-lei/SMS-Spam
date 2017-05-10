b54acf954fafde97f7d3b51115b452daf5912ea7
jason-lei <jason.lei@columbia.edu>
2017-05-09 22:59:53 -0400

    clean up of app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # #!/usr/bin/env python
 
-from __future__ import print_function # In python 2.7
-import sys
+from __future__ import print_function
 import flask
 import httplib2
 from flask import Flask, render_template, json, request, redirect
@@ -13,18 +12,13 @@ import uuid
 from googleapiclient.discovery import build
 
 app = flask.Flask(__name__)
-#drive = build('drive', 'v2', http=http_auth)
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-# @app.route('/spam-classifier')
-# def showSignUp():
-#     return render_template('classify.html')
-
-@app.route('/classify', methods=['POST'])
-def signUp():
+@app.route('/classify_text', methods=['POST'])
+def classify_text():
 
     _text = request.form['inputName']
     print(_text)
@@ -38,8 +32,9 @@ def signUp():
     print(_output)
 
     return render_template('results.html', classification=_output) 
-@app.route('/test')
-def test():
+
+@app.route('/classify_email')
+def classify_email():
     if 'credentials' not in flask.session:
         return flask.redirect(flask.url_for('oauth2callback'))
     credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
@@ -47,13 +42,8 @@ def test():
         return flask.redirect(flask.url_for('oauth2callback'))
     else:
         http_auth = credentials.authorize(httplib2.Http())
-        #service = build('gmail', 'v1', http_auth)
         service = build('gmail', 'v1', credentials=credentials)
-        #results = mail.files().list().execute()
         results = service.users().labels().list(userId='me').execute()
-        print('4')
-
-        #messages = ListMessagesMatchingQuery(service, 'me') 
         response = service.users().messages().list(userId='me',
                                                q='', maxResults=50).execute()
         messages = []
@@ -79,8 +69,6 @@ def oauth2callback():
         'client_secrets.json',
         scope='https://www.googleapis.com/auth/gmail.modify',
         redirect_uri=flask.url_for('oauth2callback', _external=True)
-        #,
-        #include_granted_scopes=True)
         )
     if 'code' not in flask.request.args:
         auth_uri = flow.step1_get_authorize_url()
@@ -89,7 +77,7 @@ def oauth2callback():
         auth_code = flask.request.args.get('code')
         credentials = flow.step2_exchange(auth_code)
         flask.session['credentials'] = credentials.to_json()
-        return flask.redirect(flask.url_for('test'))
+        return flask.redirect(flask.url_for('classify_email'))
 
 if __name__ == "__main__":
     from sklearn.externals import joblib
@@ -102,7 +90,6 @@ if __name__ == "__main__":
     vectorizer.fit(vocabulary_to_load)
     app.secret_key = str(uuid.uuid4())
     app.run(debug=True)
-    # app.run(port=8080)
 
 from oauth2client import client
 
